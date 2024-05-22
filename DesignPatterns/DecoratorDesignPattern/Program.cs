7e04d96d856cd43e2d8a0f0a9f0fee8cc8820e40
abhishek.khapekar@gmail.com <abhishek.khapekar@gmail.com>
2024-05-23 02:35:15 +0530

    End of Session 8 : Angular Basic

--- DesignPatterns/DecoratorDesignPattern/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DecoratorDesignPattern;
-
-IPizza pizza = new Pizza();
-IPizza cheeseDecorator = new CheesDecorator(pizza);
-IPizza tomatoDecorator = new TomatoDecorator(cheeseDecorator);
-IPizza OnionDecorator = new OnionDecorator(tomatoDecorator);
-IPizza paneerDecorator = new PaneerDecorator(tomatoDecorator);
-Console.WriteLine(paneerDecorator.GetPizzaType());
--- Skinet_V.0/Skinet.Client/src/app/app.component.ts
+++ Skinet_V.0/Skinet.Client/src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [
+    RouterOutlet, 
+    NavBarComponent,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Skinet';
+export class AppComponent implements OnInit {
+  title : string = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient)
+  {
+
+  }
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=20').subscribe({
+      next: (response : any) => this.products = response.data, // what to do next
+      error: error => console.log(error), // what to do if there is an error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    })
+  }
 }
